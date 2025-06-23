2f571e109a98ff6f64a7270950201e93b2f4ddb7
Firmen-net <65349576+Firmen-net@users.noreply.github.com>
2025-06-23 22:41:35 +0700

    Zone select

--- Assets/Script/UI Tween.cs
+++ Assets/Script/UI Tween.cs
@@ -10,17 +10,20 @@ public class UITween : MonoBehaviour
     [SerializeField] private float scaleDownDuration = 0.5f;
     [SerializeField] private Vector3 targetScale = new Vector3(1.2f, 1.2f, 1.2f);
     [SerializeField] private Vector3 targetPosition = new Vector3(1.2f, 1.2f, 1.2f);
+    [SerializeField] private float targetWidth;
     [SerializeField] private Ease easeType = Ease.InOutQuad;
 
     private RectTransform rectTransform;
     private Vector3 originalScale;
     private Vector3 originalTransform;
+    private float originalWidth;
 
     private void Start()
     {
         rectTransform = GetComponent<RectTransform>();
         originalScale = rectTransform.localScale;
         originalTransform = rectTransform.localPosition;
+        originalWidth = rectTransform.sizeDelta.x;
     }
 
     public void StartTweenScale()
@@ -41,5 +44,15 @@ public class UITween : MonoBehaviour
     public void StopTweenSlide()
     {
         rectTransform.DOAnchorPos(originalTransform, scaleDownDuration).SetEase(easeType);
+    }
+
+    public void StartTweenExpand()
+    {
+        rectTransform.DOSizeDelta(new Vector2(targetWidth, rectTransform.sizeDelta.y), scaleUpDuration);
+    }
+
+    public void ResetWidth()
+    {
+        rectTransform.DOSizeDelta(new Vector2(originalWidth, rectTransform.sizeDelta.y), scaleDownDuration);
     }
 }
